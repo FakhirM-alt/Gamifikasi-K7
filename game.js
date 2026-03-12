1c66c8f3e9009057c81e73c335056166b190f096
FakhirM-alt <fakhirmm29@gmail.com>
2026-03-12 21:52:26 +0700

    fix

--- game.js
+++ game.js
@@ -54,7 +54,7 @@ getNewQuestion = () => {
     if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS){
         localStorage.setItem("mostRecentScore", score);
 
-        return window.location.assign("./end");
+        return window.location.assign("./end.html");
     }
     questionCounter++;
     progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
