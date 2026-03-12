3b28394e3cf4cbc8baa78a347d224f15acf98a09
FakhirM-alt <fakhirmm29@gmail.com>
2026-03-12 10:14:36 +0700

    Update

--- end.js
+++ end.js
@@ -3,16 +3,30 @@ const saveScoreBtn = document.getElementById("saveScoreBtn");
 const finalScore = document.getElementById("finalScore");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
 
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const MAX_HIGH_SCORES = 5;
+console.log(highScores);
 
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener("input", () => {
-    console.log(username.value.length > 0);
-
     saveScoreBtn.disabled = !username.value;
 });
 
 saveHighScore = (e) => {
     console.log("Saving high score...");
     e.preventDefault();
-}
+
+    const score = {
+        score: Math.floor(Math.random() * 100),
+        name: username.value
+    };
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(5);
+
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    window.location.assign("/") ;
+
+};
