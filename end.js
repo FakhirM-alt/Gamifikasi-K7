dd90d8383563036cf1140b6b4a0bc08b068228ac
FakhirM-alt <fakhirmm29@gmail.com>
2026-03-12 21:46:02 +0700

    fixes

--- end.js
+++ end.js
@@ -26,6 +26,6 @@ saveHighScore = (e) => {
     highScores.splice(5);
 
     localStorage.setItem("highScores", JSON.stringify(highScores));
-    window.location.assign("Gamifikasi-K7/") ;
+    window.location.assign("./") ;
 
 };
