85738ce6a61b3a15e802798a9b5d218ec38ef691
FakhirM-alt <fakhirmm29@gmail.com>
2026-03-17 22:03:27 +0700

    Update

---
+++ javascript/question.js
@@ -0,0 +1,77 @@
+const questions = [
+  {
+    category: "pixelpals",
+    questions: [
+      {
+        question: "What is one of the core streams of PixelPals?",
+        options: ["Artist", "Project Manager", "Programmer", "Planner"],
+        correctAnswer: 1,
+      },
+      {
+        question: "What is the level program of Vocational Education of University of Indonesia?",
+        options: ["S1", "S2", "D3", "D4"],
+        correctAnswer: 4,
+      },
+      {
+        question: "What is the name of the PixelPals's mascot?",
+        options: ["Pixel", "Pip", "Pipa", "Pie"],
+        correctAnswer: 3,
+      },
+      {
+        question: "What is the animal of Pixelpals's mascot?",
+        options: ["Bear", "Crocodile", "Owl", "Eagle"],
+        correctAnswer: 2,
+      },
+      {
+        question: "Which color that is NOT present in PixelPals's logo?",
+        options: ["Green", "Blue", "White", "Yellow"],
+        correctAnswer: 4,
+      },
+    ],
+  },
+  {
+    category: "artist",
+    questions: [
+      {
+        question: "Which app is suitable for artist?",
+        options: ["Adobe Illustrator", "Capcut", "Adobe Audition", "Gallery"],
+        correctAnswer: 1,
+      },
+      {
+        question: "Which file type that is NOT for drawing?",
+        options: [".JPEG", ".PNG", ".MP4", ".SVG"],
+        correctAnswer: 3,
+      },
+      {
+        question: "Which of these names that AREN'T part of Adobe Illustrator?",
+        options: ["Pen Tool", "Type Tool", "Mixing Tool", "Paintbucket Tool"],
+        correctAnswer: 3,
+      },
+      {
+        question: "What is the shortcut key for Copy & Paste?",
+        options: ["CTRL + C, CTRL + V", "CTRL + A, Backspace", "CTRL + Z", "CTRL + C, CTRL + X"],
+        correctAnswer: 1,
+      },
+      {
+        question: "Which color ISN'T a secondary color?",
+        options: ["Red", "Blue", "Yellow", "Green"],
+        correctAnswer: 4,
+      }
+    ],
+  },
+  {
+    category: "designer",
+    questions: [
+      {
+      question: "What is one of the rules for a good design?",
+      options: ["Pick random colors", "Create space", "Use 4 fonts", "Be less friendly"],
+      correctAnswer: 2,
+      },
+      {
+      question: "Which of these brands uses logotype?",
+      options: ["Coca-Cola", "Google", "LG", "FedEx"],
+      correctAnswer: 4,
+      }
+    ],
+  }
+];
