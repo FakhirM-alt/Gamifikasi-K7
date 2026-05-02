d4ceda58c6231a37778472bf6e58ede5e848f5a2
FakhirM-alt <fakhirmm29@gmail.com>
2026-05-02 08:54:16 +0700

    Update

--- javascript/update.js
+++ javascript/update.js
@@ -1,13 +1,57 @@
-const highScoresList = document.getElementById("highScoresList");
+const highScoresList_1 = document.getElementById("highScoresList-PixelPals");
+const highScoresList_2 = document.getElementById("highScoresList-Designer");
+const highScoresList_3 = document.getElementById("highScoresList-Artist");
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
-const filteredScores = highScores.filter(score => score.category.includes("PixelPals"));
+const textScores = document.getElementById("textScores");
 
-highScoresList.innerHTML = filteredScores
+const filteredScore_1 = highScores.filter(score => score.category.includes("PixelPals"));
+const filteredScore_2 = highScores.filter(score => score.category.includes("Designer"));
+const filteredScore_3 = highScores.filter(score => score.category.includes("Artist"));  
+
+highScoresList_1.innerHTML = filteredScore_1
 .map( score => {
-
     return `<div class="high-score-item">
-        <li class="high-score">${score.name} - ${score.category} - ${score.score} </li>
+        <li class="high-score" id="textScores">${score.name} - ${score.category} - ${score.score} </li>
     </div>`;
 })
 .join("");
+
+highScoresList_2.innerHTML = filteredScore_2
+.map( score => {
+
+    return `<div class="high-score-item">
+        <li class="high-score" id="textScores">${score.name} - ${score.category} - ${score.score} </li>
+    </div>`;
+})
+.join("");
+
+highScoresList_3.innerHTML = filteredScore_3
+.map( score => {
+
+    return `<div class="high-score-item">
+        <li class="high-score" id="textScores">${score.name} - ${score.category} - ${score.score} </li>
+    </div>`;
+})
+.join("");
+
+const openPixelPals = document.querySelector(".PixelPals-category");
+openPixelPals.addEventListener("click", () => {
+  document.querySelector("#highScoresList-PixelPals").classList.add("active");
+    document.querySelector("#highScoresList-Designer").classList.remove("active");
+    document.querySelector("#highScoresList-Artist").classList.remove("active");
+});
+
+const openDesigner = document.querySelector(".Designer-category");
+openDesigner.addEventListener("click", () => {
+  document.querySelector("#highScoresList-Designer").classList.add("active");
+    document.querySelector("#highScoresList-PixelPals").classList.remove("active");
+    document.querySelector("#highScoresList-Artist").classList.remove("active");
+});
+
+const openArtist = document.querySelector(".Artist-category");
+openArtist.addEventListener("click", () => {
+  document.querySelector("#highScoresList-Artist").classList.add("active");
+  document.querySelector("#highScoresList-PixelPals").classList.remove("active");
+  document.querySelector("#highScoresList-Designer").classList.remove("active");
+});
